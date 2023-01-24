8ee493f572f932f2a92ae71f52557b9c396ef37d
hariharanpandi <hariharan.r.pandi@gmail.com>
2023-01-24 10:29:06 +0530

    Modified Object Name

--- trainee_time/Arraylearning.java
+++ trainee_time/Arraylearning.java
@@ -6,14 +6,14 @@ public class Arraylearning
 {
 	public static void main (String []args)
 	{
-		Scanner sc=new Scanner(System.in);
+		Scanner scanner=new Scanner(System.in);
 		int[] arr=new int[5];
 		
 		for(int i=0;i<=arr.length;i++)
 		{
 			System.out.print("Enter the number : ");
 			try {
-				arr[i]=sc.nextInt();	
+				arr[i]=scanner.nextInt();	
 			}
 			catch(ArrayIndexOutOfBoundsException nfe)
 			{
